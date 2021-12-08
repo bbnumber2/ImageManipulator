e7390ec9847635df8499a2dbdd551b80c8e1e98e
bbnumber2 <caleb.m.brinkman@gmail.com>
2021-12-08 17:44:55 -0500

    chore: Remove unnecessary imports

--- src/ImageManipulator.java
+++ src/ImageManipulator.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,7 +17,6 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ImageManipulator extends Application implements ImageManipulatorInterface{
